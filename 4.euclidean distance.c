49f1ce58123eec434ffde49f4b433397918c2593
2023A3R011-COM111 <146457495+2023A3R011-COM111@users.noreply.github.com>
2023-11-28 11:16:41 +0530

    Update 4.euclidean distance.c

--- 4.euclidean distance.c
+++ 4.euclidean distance.c
@@ -2,13 +2,13 @@
 #include<math.h>
 int main ()
 {
-    float x1 ,y1,x2,y2,distance;
+    float x1,y1,x2,y2,Euclidean_Distance;
     printf("Enter the coordinates of first point");
-    scanf("%f",& x1, &y1);
+    scanf("%f %f",& x1, &y1);
     printf("Enter the coordinates of second point");
-    scanf("%f", & x2,&y2);
+    scanf("%f %f", & x2,&y2);
 
-    distance = sqrt(pow(x2-x1, 2)+ pow(y2-y1,2));
-    printf("The value of Distance between the two points is: %f\n",distance);
+    Euclidean_Distance = sqrt(pow(x2-x1, 2)+ pow(y2-y1,2));
+    printf("The value of Distance between the two points is: %f\n",Euclidean_Distance);
     return 0;
 }
