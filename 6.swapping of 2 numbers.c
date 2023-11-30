616d5513e65d43c855368955522225a3a4af4747
2023A3R011-COM111 <146457495+2023A3R011-COM111@users.noreply.github.com>
2023-11-30 22:09:54 +0530

    Update 6.swapping of 2 numbers.c

--- 6.swapping of 2 numbers.c
+++ 6.swapping of 2 numbers.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
+     // Program for swapping two variables without using a third variable
     int a, b;
-    printf("Enter the two numbers : \n");
+    printf("Enter the two numbers : ");
     scanf("%d %d", &a, &b);
-    printf("Before swapping A is : %d and B is %d \n", a,b);
+    printf("Before swapping :- value 1 is : %d and value 2 is %d \n", a,b);
  
-    // Swapping without using a third variable
     // Sum of both numbers is stored in 'a'
     a = a + b;
     // Difference of sum and original 'b' is stored in 'b'
@@ -14,7 +14,7 @@ int main()
     // Difference of sum and new 'b' is stored in 'a'
     a = a - b;
  
-    printf("After swapping A is : %d and B is : %d \n", a, b);
+    printf("After swapping :- value 1 is : %d and value 2 is : %d \n", a, b);
     return 0;
 }
     
