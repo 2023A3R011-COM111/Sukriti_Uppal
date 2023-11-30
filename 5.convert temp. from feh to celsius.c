aa7cc3353bc7f74aff3ee672647159370111045c
2023A3R011-COM111 <146457495+2023A3R011-COM111@users.noreply.github.com>
2023-11-30 22:01:41 +0530

    Update 5.convert temp. from feh to celsius.c

--- 5.convert temp. from feh to celsius.c
+++ 5.convert temp. from feh to celsius.c
@@ -2,11 +2,13 @@
 #include<math.h>
 int main ()
 {
-    float a,b;
-    //Enter the value of temperature in Fahrenheit
-    printf("Enter the value of temperature in Fahrenheit");
-    scanf("%f",&a);
-    b=5.0/9.0*(a-32);
-    printf ("The value of temperature in celsius is %f",b);
+    float feh,cel;
+    //Iput from user: temperature in Fahrenheit
+    printf("Enter the value of temperature in Fahrenheit: ");
+    scanf("%f",&feh);
+    //Formula and calculation
+    cel=5.0/9.0*(feh-32);
+    printf ("The value of temperature in celsius is %f",cel);
     return 0;
 }
+
