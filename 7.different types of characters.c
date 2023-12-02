534245ca1d054516f7fa93d81a3c156524b929bf
2023A3R011-COM111 <146457495+2023A3R011-COM111@users.noreply.github.com>
2023-12-02 22:47:41 +0530

    Update 7.different types of characters.c

--- 7.different types of characters.c
+++ 7.different types of characters.c
@@ -1,37 +1,39 @@
 #include<stdio.h>
  int main ()
  {
+    // Program of list of characters in char datatype
+
      char singleQuote = '\''; // Single quote character
      printf("Single Quote: %c\n", singleQuote);
 
-    char doubleQuote = '\"'; // double quote character
+    char doubleQuote = '\"'; // Double quote character
      printf("Double Quote: %c\n", doubleQuote);
 
      char backslash = '\\';// Backslash character
      printf("Backslash: %c\n", backslash);
 
-     printf ("Hello\nWorld\n"); // New line (line feed) character
-
-     printf("Tabbed\tHii\tHow\tare\tyou\t\n");
-
-     printf("Vertical\vTab\vCharacter\vDisplay\n");
-
-     printf("remove a\b char\n");
-    
-     printf("Carriage\rReturn\n");
-    
-     printf("Form\fFeed\n");
-
-     printf("Beep\a\n");
-    
-     char nullChar =  '\0';
+     char nullChar =  '\0';// Null character
      printf("Null Character: %c\n", nullChar);
     
-     char questionMark = '\?';
+     char questionMark = '\?';// Question Mark character
      printf("Question Mark: %c\n", questionMark);
      
-     char hexvalue = '\x17';
+     char hexvalue = '\x17';// Hexvalue character
      printf("Hex Value: %c\n",hexvalue);
+
+     printf ("Hello\nWorld\n"); // New line (line feed) character
+
+     printf("Tabbed\tHii\tHow\tare\tyou\t\n");// Space of tab- tab character
+
+     printf("Vertical\vTab\vCharacter\vDisplay\n");// Vertical tab character
+
+     printf("remove a\b char\n");// Remove character
+    
+     printf("Carriage\rReturn\n");// Return character
+    
+     printf("Form\fFeed\n");// Feed character
+
+     printf("Beep\a\n");// Beep sound character
     
      return 0;
  }
